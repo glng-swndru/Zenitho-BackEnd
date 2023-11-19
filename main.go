f16f85c1063153adeaacf52b6038f6ff97495063
gilang swandaru <gilangswandaru27@gmail.com>
2023-11-20 06:43:18 +0800

    test database

--- main.go
+++ main.go
@@ -3,40 +3,18 @@ package main
 import (
 	"campaignku/user"
 	"log"
-	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// dsn := "root:Br41nsc00l#2023@tcp(127.0.0.1:3306)/campaignku?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("Connection to database is good")
-
-	// var users []user.User
-
-	// db.Find(&users)
-
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// 	fmt.Println("===============")
-	// }
-
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-}
-
-func handler(c *gin.Context) {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbHost := os.Getenv("DB_HOST")
@@ -49,8 +27,11 @@ func handler(c *gin.Context) {
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
-	db.Find(&users)
+	userRepository := user.NewRepository(db)
+	user := user.User{
+		Name: "test simpan",
+	}
 
-	c.JSON(http.StatusOK, users)
+	userRepository.Save(user)
+
 }
--- user/repository.go
+++ user/repository.go
@@ -1,5 +1,32 @@
 package user
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Repository interface {
 	Save(user User) (User, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) Save(user User) (User, error) {
+
+	now := time.Now()
+	user.CreateAt = now
+	user.UpdateAt = now
+
+	err := r.db.Create(&user).Error
+	if err != nil {
+		return user, err
+	}
+	return user, nil
 }
