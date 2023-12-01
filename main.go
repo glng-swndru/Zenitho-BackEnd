0303e405d6864828971d214e305fbccb293ef382
gilang swandaru <gilangswandaru27@gmail.com>
2023-12-01 16:13:50 +0800

    VALID

--- main.go
+++ main.go
@@ -37,6 +37,23 @@ func main() {
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 
+	token, err := authService.ValidateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxfQ.qbaY_7srFbATRi7GAKOGtQ0bJLujtxRwwS3oP-eCMu0")
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println("ERROR")
+		fmt.Println("ERROR")
+	}
+
+	if token.Valid {
+		fmt.Println("VALID")
+		fmt.Println("VALID")
+		fmt.Println("VALID")
+	} else {
+		fmt.Println("INVALID")
+		fmt.Println("INVALID")
+		fmt.Println("INVALID")
+	}
+
 	fmt.Println(authService.GenerateToken(1001))
 
 	userService.SaveAvatar(1, "images/1-profile.png")
