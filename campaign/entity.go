7af35854a77f9ddc4d6345ae358a8e33d90c09ab
gilang swandaru <gilangswandaru27@gmail.com>
2023-12-03 10:12:40 +0800

    campaign entity

--- campaign/entity.go
+++ campaign/entity.go
@@ -1,1 +1,27 @@
 package campaign
+
+import "time"
+
+type Campaign struct {
+	ID               int
+	UserId           int
+	Name             string
+	ShortDescription string
+	Description      string
+	Perks            string
+	BackerCount      int
+	GoalAmount       int
+	CurrentAmount    int
+	Slug             string
+	CreatedAt        time.Time
+	UpdateAt         time.Time
+}
+
+type CampaignImage struct {
+	ID         int
+	CampaignID int
+	FileName   string
+	IsPrimary  int
+	CreatedAt  time.Time
+	UpdateAt   time.Time
+}
